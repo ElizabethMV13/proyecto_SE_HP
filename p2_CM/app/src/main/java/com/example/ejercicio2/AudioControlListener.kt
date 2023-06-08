bb1b1a38efb75bd279cb19d0a8a69f3224da1907
ElizabethMV13 <dulce.mendoza@ingenieria.unam.edu>
2023-06-07 20:44:39 -0600

    cambios

--- p2_CM/app/src/main/java/com/example/ejercicio2/AudioControlListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ejercicio2
-
-interface AudioControlListener {
-    fun pauseAudio()
-    fun resumeAudio()
-}
--- p2_CM/app/src/main/java/com/example/ejercicio2/utils/Constans.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ejercicio2.utils
-
-object Constants{
-    const val BASE_URL = "https://hp-api.onrender.com/"     // api/character/
-    const val LOGTAG = "LOGS"
-}
